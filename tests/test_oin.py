bca89a5aad3476bcf40cac465a7eda3eb70b54eb
Arnur Nigmetov <anigmetov@lbl.gov>
2021-02-11 22:50:11 -0800

    Add script to compare with Dionysus

---
+++ tests/test_oin.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import argh
+import argparse
+import numpy as np
+
+import oineus as oin
+import dionysus as dion
+
+
+def compare_diagrams(n=16, negate=False, n_threads=4, seed=1, top_dim=2):
+    # no wrap in Dionysus
+    wrap = False
+
+    # generate random grid data
+    np.random.seed(seed)
+    a = np.random.randn(n ** 3).reshape((n, n, n))
+
+    # compute diagrams with Oineus
+    oin_dgms = oin.compute_diagrams_ls(a, negate, wrap, top_dim, n_threads)
+
+    # compute diagrams with Dionysis
+    fil_us = dion.fill_freudenthal(a, reverse=negate)
+    p = dion.homology_persistence(fil_us)
+    dion_dgms = dion.init_diagrams(p, fil_us)
+
+    dist = 0.0
+
+    for dim in range(top_dim):
+        # convert Oineus diagram to Dionysus format
+        oin_dgm = dion.Diagram(oin_dgms[dim])
+        dion_dgm = dion_dgms[dim]
+        dist += dion.bottleneck_distance(oin_dgm, dion_dgm)
+
+    print("total dist: ", dist)
+    assert(dist < 0.001)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    argh.add_commands(parser, [compare_diagrams])
+    argh.dispatch(parser)
