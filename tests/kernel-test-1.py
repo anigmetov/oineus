dd79e069f4d3c93f36c641cfb2d2cfc96867d9dc
Yossi Bokor Bleile <yossi@yossi.eu>
2023-06-21 12:34:21 -0700

    Interim

--- tests/kernel-test-1.py
+++ tests/kernel-test-1.py
@@ -2,15 +2,15 @@ import oineus as oin
 import math
 
 def test_kernel_1():
-	params=oin.ReductionParams()
-	params.n_threads=4
-	params.kernel=True
-	params.image=True
-	params.cokernel=True
-	K = [ [0, [0], 10], [1,[1],50], [2,[2], 20], [3, [3], 50], [4,[4], 15], [5, [5], 12], [6,[0,1], 50], [7, [1,2], 60], [8,[2,3], 70], [9, [3,4], 80], [10, [0,5], 30], [11,[4,5], 20]]
-	L = [ [0, [0], 10], [1,[1],50], [2,[2], 20], [3, [3], 50], [4,[4], 15], [5,[0,1], 50], [6, [1,2], 60], [7,[2,3], 70], [8, [3,4], 80] ]
-	IdMapping = [0,1,2,3,4,6,7,8,9]
-	kicr = oin.compute_kernel_image_cokernel_reduction(K, L, IdMapping, params)
+params=oin.ReductionParams()
+params.n_threads=4
+params.kernel=True
+params.image=True
+params.cokernel=True
+K = [ [0, [0], 10], [1,[1],50], [2,[2], 20], [3, [3], 50], [4,[4], 15], [5, [5], 12], [6,[0,1], 50], [7, [1,2], 60], [8,[2,3], 70], [9, [3,4], 80], [10, [0,5], 30], [11,[4,5], 20]]
+L = [ [0, [0], 10], [1,[1],50], [2,[2], 20], [3, [3], 50], [4,[4], 15], [5,[0,1], 50], [6, [1,2], 60], [7,[2,3], 70], [8, [3,4], 80] ]
+IdMapping = [0,1,2,3,4,6,7,8,9]
+kicr = oin.compute_kernel_image_cokernel_reduction(K, L, IdMapping, params)
 	errors = False
 	if (kicr.kernel_diagrams().in_dimension(0) != [[30., 80.],[20., math.inf]]).any():
 		print("Error in kernel_test_1: kernel diagram in dimenion 0 incorrect.")
