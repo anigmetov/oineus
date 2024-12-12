32b0efecf1a3ee189b3dd882bd95ac94ee8e2a85
Arnur Nigmetov <anigmetov@lbl.gov>
2024-12-12 00:19:00 -0800

    Add missing cpp files

---
+++ bindings/python/oineus_cells.cpp
@@ -0,0 +1,114 @@
+#include "oineus_persistence_bindings.h"
+
+void init_oineus_cells(py::module& m)
+{
+    using namespace pybind11::literals;
+
+    using Simplex = oin::Simplex<oin_int>;
+    using SimplexValue = oin::CellWithValue<oin::Simplex<oin_int>, oin_real>;
+
+    using ProdSimplex = oin::ProductCell<Simplex, Simplex>;
+    using ProdSimplexValue = oin::CellWithValue<ProdSimplex, oin_real>;
+    using ProdFiltration = oin::Filtration<ProdSimplex, oin_real>;
+
+    // Simplex and product of two simplices without value, vertices only
+    const std::string pure_simplex_class_name = "CombinatorialSimplex";
+    const std::string pure_prod_simplex_class_name = "CombinatorialProdSimplex";
+
+    const std::string simplex_class_name = "Simplex";
+    const std::string prod_simplex_class_name = "ProdSimplex";
+
+    py::class_<Simplex>(m, pure_simplex_class_name.c_str())
+        .def(py::init([](Simplex::IdxVector vs) -> Simplex { return Simplex(vs, true); }), py::arg("vertices"))
+        .def(py::init([](oin_int id, Simplex::IdxVector vs) -> Simplex { return Simplex(id, vs, true); }), py::arg("id"), py::arg("vertices"))
+        .def("__iter__", [](Simplex& sigma) { return py::make_iterator(sigma.vertices_.begin(), sigma.vertices_.end()); }, py::keep_alive<0, 1>())
+        .def("__getitem__", [](Simplex& sigma, size_t i) { return sigma.vertices_[i]; })
+        .def_property("id", &Simplex::get_id, &Simplex::set_id)
+        .def_readonly("vertices", &Simplex::vertices_)
+        .def_property_readonly("uid", &Simplex::get_uid)
+        .def("dim", &Simplex::dim)
+        .def("boundary", &Simplex::boundary)
+        .def("join", [](const Simplex& sigma, oin_int new_vertex, oin_int new_id) {
+                  return sigma.join(new_id, new_vertex);
+                },
+                py::arg("new_vertex"),
+                py::arg("new_id") = Simplex::k_invalid_id)
+        .def("__repr__", [](const Simplex& sigma) {
+          std::stringstream ss;
+          ss << sigma;
+          return ss.str();
+        });
+
+    py::class_<ProdSimplex>(m, pure_prod_simplex_class_name.c_str())
+        .def(py::init([](Simplex::IdxVector vs1, Simplex::IdxVector vs2) -> ProdSimplex
+            { return ProdSimplex(Simplex(vs1, true), Simplex(vs2, true)); }), py::arg("vertices_1"), py::arg("vertices_2"))
+        .def_property("id", &ProdSimplex::get_id, &ProdSimplex::set_id)
+        .def_property_readonly("factor_1", &ProdSimplex::get_factor_1)
+        .def_property_readonly("factor_2", &ProdSimplex::get_factor_2)
+        .def_property_readonly("uid", &ProdSimplex::get_uid)
+        .def("dim", &ProdSimplex::dim)
+        .def("boundary", &ProdSimplex::boundary)
+        .def("__repr__", [](const ProdSimplex& sigma) {
+          std::stringstream ss;
+          ss << sigma;
+          return ss.str();
+        });
+
+    py::class_<SimplexValue>(m, simplex_class_name.c_str())
+            .def(py::init([](Simplex::IdxVector vs, oin_real value) -> SimplexValue {
+                      return SimplexValue({vs}, value);
+                    }),
+                    py::arg("vertices"),
+                    py::arg("value")=0.0)
+            .def(py::init([](oin_int id, Simplex::IdxVector vs, oin_real value) -> SimplexValue { return SimplexValue({id, vs}, value); }), py::arg("id"), py::arg("vertices"), py::arg("value"))
+            .def("__iter__", [](SimplexValue& sigma) { return py::make_iterator(sigma.cell_.vertices_.begin(), sigma.cell_.vertices_.end()); }, py::keep_alive<0, 1>())
+            .def("__getitem__", [](SimplexValue& sigma, size_t i) { return sigma.cell_.vertices_[i]; })
+            .def_property("id", &SimplexValue::get_id, &SimplexValue::set_id)
+            .def_readwrite("sorted_id", &SimplexValue::sorted_id_)
+            .def_property_readonly("vertices", [](const SimplexValue& sigma) { return sigma.cell_.vertices_; })
+            .def_property_readonly("uid", &SimplexValue::get_uid)
+            .def_readwrite("value", &SimplexValue::value_)
+            .def("dim", &SimplexValue::dim)
+            .def("boundary", &SimplexValue::boundary)
+            .def("combinatorial_simplex", &SimplexValue::get_cell)
+            .def("combinatorial_cell", &SimplexValue::get_cell)
+            .def("join", [](const SimplexValue& sigma, oin_int new_vertex, oin_real value, oin_int new_id) {
+                      return sigma.join(new_id, new_vertex, value);
+                    },
+                    py::arg("new_vertex"),
+                    py::arg("value"),
+                    py::arg("new_id") = SimplexValue::k_invalid_id)
+            .def("__repr__", [](const SimplexValue& sigma) {
+              std::stringstream ss;
+              ss << sigma;
+              return ss.str();
+            });
+
+    py::class_<ProdSimplexValue>(m, prod_simplex_class_name.c_str())
+            .def(py::init([](const SimplexValue& sigma, const SimplexValue& tau, oin_real value) -> ProdSimplexValue {
+                      return ProdSimplexValue(ProdSimplex(sigma.get_cell(), tau.get_cell()), value);
+                    }),
+                    py::arg("cell_1"),
+                    py::arg("cell_2"),
+                    py::arg("value"))
+            .def(py::init([](const Simplex& sigma, const Simplex& tau, oin_real value) -> ProdSimplexValue {
+                      return ProdSimplexValue(ProdSimplex(sigma, tau), value);
+                    }),
+                    py::arg("cell_1"),
+                    py::arg("cell_2"),
+                    py::arg("value"))
+            .def_property("id", &ProdSimplexValue::get_id, &ProdSimplexValue::set_id)
+            .def_readwrite("sorted_id", &ProdSimplexValue::sorted_id_)
+            .def_property_readonly("cell_1", &ProdSimplexValue::get_factor_1)
+            .def_property_readonly("cell_2", &ProdSimplexValue::get_factor_2)
+            .def_property_readonly("uid", &ProdSimplexValue::get_uid)
+            .def_readwrite("value", &ProdSimplexValue::value_)
+            .def("dim", &ProdSimplexValue::dim)
+            .def("boundary", &ProdSimplexValue::boundary)
+            .def("combinatorial_cell", &ProdSimplexValue::get_cell)
+            .def("__repr__", [](const ProdSimplexValue& sigma) {
+              std::stringstream ss;
+              ss << sigma;
+              return ss.str();
+            });
+}
---
+++ bindings/python/oineus_filtration.cpp
@@ -0,0 +1,105 @@
+#include "oineus_persistence_bindings.h"
+
+void init_oineus_filtration(py::module& m)
+{
+    using namespace pybind11::literals;
+
+    using DgmPoint = typename oin::Diagrams<oin_real>::Point;
+
+    using Simplex = oin::Simplex<oin_int>;
+    using Filtration = oin::Filtration<Simplex, oin_real>;
+
+    using ProdSimplex = oin::ProductCell<Simplex, Simplex>;
+    using ProdFiltration = oin::Filtration<ProdSimplex, oin_real>;
+
+    using oin::VREdge;
+
+    const std::string filtration_class_name = "Filtration";
+    const std::string prod_filtration_class_name = "ProdFiltration";
+
+    py::class_<Filtration>(m, filtration_class_name.c_str())
+            .def(py::init<Filtration::CellVector, bool, int, bool, bool>(),
+                    py::arg("cells"),
+                    py::arg("negate") = false,
+                    py::arg("n_threads") = 1,
+                    py::arg("sort_only_by_dimension") = false,
+                    py::arg("set_ids") = true)
+            .def("__len__", &Filtration::size)
+            .def("__iter__", [](Filtration& fil) { return py::make_iterator(fil.begin(), fil.end()); }, py::keep_alive<0, 1>())
+            .def("__getitem__", [](Filtration& fil, int i) { if (i < 0) i = fil.size() + i; return fil.get_cell(i);})
+            .def("max_dim", &Filtration::max_dim, "maximal dimension of a cell in filtration")
+            .def("cells", &Filtration::cells_copy, "copy of all cells in filtration order")
+            .def("simplices", &Filtration::cells_copy, "copy of all simplices (cells) in filtration order")
+            .def("size", &Filtration::size, "number of cells in filtration")
+            .def("size_in_dimension", &Filtration::size_in_dimension, py::arg("dim"), "number of cells of dimension dim")
+            .def("n_vertices", &Filtration::n_vertices)
+            .def("simplex_value_by_sorted_id", &Filtration::value_by_sorted_id, py::arg("sorted_id"))
+            .def("id_by_sorted_id", &Filtration::get_id_by_sorted_id, py::arg("sorted_id"))
+            .def("sorted_id_by_id", &Filtration::get_sorted_id, py::arg("id"))
+            .def("cell", &Filtration::get_cell, py::arg("i"))
+            .def("simplex", &Filtration::get_cell, py::arg("i"))
+            .def("sorting_permutation", &Filtration::get_sorting_permutation)
+            .def("inv_sorting_permutation", &Filtration::get_inv_sorting_permutation)
+            .def("value_by_uid", &Filtration::value_by_uid, py::arg("uid"))
+            .def("sorted_id_by_uid", &Filtration::get_sorted_id_by_uid, py::arg("uid"))
+            .def("cell_by_uid", &Filtration::get_cell_by_uid, py::arg("uid"))
+            .def("boundary_matrix", &Filtration::boundary_matrix, py::arg("n_threads")=1)
+            .def("boundary_matrix_in_dimension", &Filtration::boundary_matrix_in_dimension, py::arg("dim"), py::arg("n_threads")=1)
+            .def("coboundary_matrix", &Filtration::coboundary_matrix, py::arg("n_threads")=1)
+            .def("boundary_matrix_rel", &Filtration::boundary_matrix_rel)
+            .def("reset_ids_to_sorted_ids", &Filtration::reset_ids_to_sorted_ids)
+            .def("set_values", &Filtration::set_values)
+            .def("subfiltration", [](Filtration& self, const py::function& py_pred) {
+                auto pred = [&py_pred](const typename Filtration::Cell& s) -> bool { return py_pred(s).template cast<bool>(); };
+                Filtration result = self.subfiltration(pred);
+                return result;
+            }, py::arg("predicate"), py::return_value_policy::move)
+            .def("__repr__", [](const Filtration& fil) {
+              std::stringstream ss;
+              ss << fil;
+              return ss.str();
+            });
+
+    py::class_<ProdFiltration>(m, prod_filtration_class_name.c_str())
+            .def(py::init<ProdFiltration::CellVector, bool, int, bool, bool>(),
+                    py::arg("cells"),
+                    py::arg("negate") = false,
+                    py::arg("n_threads") = 1,
+                    py::arg("sort_only_by_dimension") = false,
+                    py::arg("set_ids") = true)
+            .def("__len__", &ProdFiltration::size)
+            .def("__iter__", [](ProdFiltration& fil) { return py::make_iterator(fil.begin(), fil.end()); }, py::keep_alive<0, 1>())
+            .def("__getitem__", [](ProdFiltration& fil, size_t i) { return fil.get_cell(i);})
+            .def("max_dim", &ProdFiltration::max_dim, "maximal dimension of a cell in filtration")
+            .def("cells", &ProdFiltration::cells_copy, "copy of all cells in filtration order")
+            .def("size", &ProdFiltration::size, "number of cells in filtration")
+            .def("size_in_dimension", &ProdFiltration::size_in_dimension, py::arg("dim"), "number of cells of dimension dim")
+            .def("cell_value_by_sorted_id", &ProdFiltration::value_by_sorted_id, py::arg("sorted_id"))
+            .def("get_id_by_sorted_id", &ProdFiltration::get_id_by_sorted_id, py::arg("sorted_id"))
+            .def("get_sorted_id_by_id", &ProdFiltration::get_sorted_id, py::arg("id"))
+            .def("get_cell", &ProdFiltration::get_cell, py::arg("i"))
+            .def("get_sorting_permutation", &ProdFiltration::get_sorting_permutation)
+            .def("get_inv_sorting_permutation", &ProdFiltration::get_inv_sorting_permutation)
+            .def("boundary_matrix", &ProdFiltration::boundary_matrix, py::arg("n_threads"))
+            .def("boundary_matrix_in_dimension", &ProdFiltration::boundary_matrix_in_dimension, py::arg("dim"), py::arg("n_threads"))
+            .def("coboundary_matrix", &ProdFiltration::coboundary_matrix, py::arg("n_threads"))
+            .def("reset_ids_to_sorted_ids", &ProdFiltration::reset_ids_to_sorted_ids)
+            .def("set_values", &ProdFiltration::set_values)
+            .def("__repr__", [](const ProdFiltration& fil) {
+              std::stringstream ss;
+              ss << fil;
+              return ss.str();
+            });
+
+    m.def("_mapping_cylinder", &oin::build_mapping_cylinder<Simplex, oin_real>, py::arg("fil_domain"), py::arg("fil_codomain"), py::arg("v_domain"), py::arg("v_codomain"), "return mapping cylinder filtration of inclusion fil_domain -> fil_codomain, fil_domain is multiplied by v_domain and fil_codomain is multiplied by v_codomain");
+    m.def("_mapping_cylinder_with_indices", &oin::build_mapping_cylinder_with_indices<Simplex, oin_real>, py::arg("fil_domain"), py::arg("fil_codomain"), py::arg("v_domain"), py::arg("v_codomain"), "return mapping cylinder filtration of inclusion fil_domain -> fil_codomain, fil_domain is multiplied by v_domain and fil_codomain is multiplied by v_codomain");
+    m.def("_multiply_filtration", &oin::multiply_filtration<Simplex, oin_real>, py::arg("fil"), py::arg("sigma"), "return a filtration with each simplex in fil multiplied by simplex sigma");
+
+    m.def("_min_filtration", &oin::min_filtration<Simplex, oin_real>, py::arg("fil_1"), py::arg("fil_2"), "return a filtration where each simplex has minimal value from fil_1, fil_2");
+    m.def("_min_filtration", &oin::min_filtration<ProdSimplex, oin_real>, py::arg("fil_1"), py::arg("fil_2"), "return a filtration where each cell has minimal value from fil_1, fil_2");
+
+    // helper for differentiable filtration
+    m.def("_min_filtration_with_indices", &oin::min_filtration_with_indices<Simplex, oin_real>, py::arg("fil_1"), py::arg("fil_2"), "return a tuple (filtration, inds_1, inds_2) where each simplex has minimal value from fil_1, fil_2 and inds_1, inds_2 are its indices in fil_1, fil_2");
+    m.def("_min_filtration_with_indices", &oin::min_filtration_with_indices<ProdSimplex, oin_real>, py::arg("fil_1"), py::arg("fil_2"), "return a tuple (filtration, inds_1, inds_2) where each simplex has minimal value from fil_1, fil_2 and inds_1, inds_2 are its indices in fil_1, fil_2");
+
+}
